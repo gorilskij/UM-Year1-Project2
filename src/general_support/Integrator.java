70034e49917a9afab3277aa59d3a47e41e5c2e8e
Vagab <bilyksergeaa@gmail.com>
2019-06-12 20:53:16 +0200

    add some more interfaces and javadoc

---
+++ src/general_support/Integrator.java
@@ -0,0 +1,12 @@
+package general_support;
+
+public interface Integrator {
+
+    /**
+     *
+     * @param position Vector indicating current position
+     * @param acceleration Vector indicating current acceleration
+     * @param velocity Vector indicating current velocity
+     */
+    void integrate(Vector position, Vector acceleration, Vector velocity);
+}
--- src/simulation/Simulation.java
+++ src/simulation/Simulation.java
@@ -1,6 +1,34 @@
 package simulation;
 
+import body.interfaces.Body;
+import simulation.universe.Universe;
+
+
 public interface Simulation {
+    /**
+     *
+     * @param bodies celestial objects of the universe
+     */
+    void init(Body ...bodies);
+
+    /**
+     *
+     * @param universe in our case solar-system
+     */
+    void iterate(Universe universe);
+
+    /**
+     *
+     * @return current timeline value
+     */
+    double time();
+
+    /**
+     * if we need to use change in time
+     * @return time step
+     */
+    double timeStep();
+
 
 }
 
