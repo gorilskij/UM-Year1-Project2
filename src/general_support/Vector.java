59a57b6a7568442900b45319132c58ef313dd694
gorilskij <43991400@users.noreply.github.com>
2019-06-12 14:09:58 +0200

    implement vector

---
+++ src/general_support/Vector.java
@@ -0,0 +1,82 @@
+package general_support;
+
+import java.util.Locale;
+
+public class Vector {
+    public final double x, y, z;
+
+    public Vector(double x, double y, double z) {
+        // TODO: maybe remove for performance
+        if (Double.isNaN(x) || Double.isNaN(y) || Double.isNaN(z))
+            throw new IllegalStateException("tried to construct a vector with NaN: " + x + ", " + y + ", " + z);
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double magnitude() {
+        return Math.sqrt(x*x + y*y + z*z);
+    }
+
+    public Vector direction() {
+        double mag = magnitude();
+        return new Vector(
+                x / mag,
+                y / mag,
+                z / mag
+        );
+    }
+
+    public Vector times(double constant) {
+        return new Vector(
+                x * constant,
+                y * constant,
+                z * constant
+        );
+    }
+
+    public Vector plus(Vector other) {
+        return new Vector(
+                x + other.x,
+                y + other.y,
+                z + other.z
+        );
+    }
+
+    public Vector minus(Vector other) {
+        return new Vector(
+                x - other.x,
+                y - other.y,
+                z - other.z
+        );
+    }
+
+    public Vector inverse() {
+        return new Vector(-x, -y, -z);
+    }
+
+    public Vector vectorTo(Vector other) {
+        return new Vector(
+                other.x - x,
+                other.y - y,
+                other.z - z
+        );
+    }
+
+    public double distanceTo(Vector other) {
+        return Math.sqrt(
+                Math.pow(x - other.x, 2)
+                        + Math.pow(y - other.y, 2)
+                        + Math.pow(z - other.z, 2)
+        );
+    }
+
+    public String toString() {
+        return String.format(
+                Locale.US,
+                "(%.1e, %.1e, %.1e)",
+                x, y, z
+        );
+    }
+}
